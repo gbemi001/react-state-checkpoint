076d1cf1f813f87c4b387f2b1eb799e0fbc172fd
Yoloye Gbemi <yoloyegbemi@gmail.com>
2022-01-30 23:42:04 +0100

    new change

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import React, { useState } from 'react';
 
 class App extends React.Component {
   state = {
-    imgSrc: "./public\gbemiYoloye.jpg",
-    fullName: "Oluwagbemi Yoloye",
-    profession: "Football Coach",
-    bio: "Learned Full Stack JS at GoMyCode",
-    shows: true
+    imgSrc: "",
+    fullName: "",
+    profession: "",
+    bio: ""
   }
+
   
-  handleToggle = () => this.setState({shows:false})
+  handleToggle = () => this.setState({imgSrc:"./public/gbemiYoloye.jpg",
+  fullName: "Oluwagbemi Yoloye",
+  profession: "Football Coach",
+  bio: "Learned Full Stack JS at GoMyCode",
+  })
   
   render () {
     return (
     <div className="App">
       <div>
         <button onClick = {this.handleToggle}><input type="checkbox"/></button>
-        {this.state.shows}
       </div>
       <div>{this.state.imgSrc}</div>
       <h1>{this.state.fullName}</h1>
